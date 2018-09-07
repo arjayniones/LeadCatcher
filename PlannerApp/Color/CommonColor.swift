61f515799f8d0e855a7a54845b9d08bba95ac9fd
Lim Ai Zhi <aizhi.lim@sicmsb.com>
2018-09-07 11:07:48 +0800

    complete sign up VC UI.

--- PlannerApp/Color/CommonColor.swift
+++ PlannerApp/Color/CommonColor.swift
@@ -11,5 +11,7 @@ import UIKit
 
 struct CommonColor {
     static let buttonBlackColor:UIColor = UIColor(red: 74/255.0, green: 74/255.0, blue: 74/255.0, alpha: 1.0);
+    static let naviBarBlackColor:UIColor = UIColor(red: 60/255.0, green: 60/255.0, blue: 60/255.0, alpha: 1.0);
+    static let systemWhiteColor:UIColor = UIColor.white;
     
 }
