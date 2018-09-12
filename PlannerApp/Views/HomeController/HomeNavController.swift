ac12b156edca6e485841d0209be4125f8cea4a8a
rjalkuino <robertjohn.alkuino@sicmsb.com>
2018-09-12 17:25:48 +0800

    added homeviewcontroller

--- PlannerApp/Views/HomeController/HomeNavController.swift
+++ PlannerApp/Views/HomeController/HomeNavController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class HomeNavController: BaseNavigationController {
 
-    let controller = UIViewController()
+    let controller = HomeViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +25,5 @@ class HomeNavController: BaseNavigationController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ PlannerApp/Views/HomeController/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  PlannerApp
+//
+//  Created by Alkuino Robert John Matias on 12/09/2018.
+//  Copyright © 2018 SICMSB. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    let calendarView = UIView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        
+        calendarView.frame = CGRect(x: 0, y: 0, width: view.width, height: view.height/2)
+        calendarView.backgroundColor = .red
+        view.addSubview(calendarView)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
