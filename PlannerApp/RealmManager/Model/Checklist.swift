2b90b4d6f4070d057e5f84f60359f480750487b8
rjalkuino <robertjohn.alkuino@sicmsb.com>
2018-10-26 14:59:13 +0800

    checklist added

---
+++ PlannerApp/RealmManager/Model/Checklist.swift
@@ -0,0 +1,25 @@
+//
+//  Checklist.swift
+//  PlannerApp
+//
+//  Created by Alkuino Robert John Matias on 26/10/2018.
+//  Copyright © 2018 SICMSB. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+import RealmSwift
+
+class Checklist: Model {
+    @objc dynamic var title: String = "";
+    @objc dynamic var status: String = "";
+    
+    func newInstance() {
+        id = uuid()
+        created_at = Date()
+        updated_at = Date()
+    }
+    
+}
+
+
