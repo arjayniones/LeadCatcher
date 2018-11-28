1e1ad39d4caf84f56198bb7e64073951a4c5faf0
rjalkuino <robertjohn.alkuino@sicmsb.com>
2018-11-28 09:46:53 +0800

    changes in calendar

---
+++ PlannerApp/New Group/CalendarView.swift
@@ -0,0 +1,24 @@
+//
+//  CalendarView.swift
+//  PlannerApp
+//
+//  Created by Alkuino Robert John Matias on 27/11/2018.
+//  Copyright © 2018 SICMSB. All rights reserved.
+//
+
+import UIKit
+
+class CalendarView: UIView {
+    
+    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
