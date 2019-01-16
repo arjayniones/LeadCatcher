edc0a4e1dd27c5aaf504bca581da48b904199d22
aizhi.lim@sicmsb.com <aizhi.lim@sicmsb.com>
2019-01-16 09:29:27 +0800

    completed on landing and welcome screen. fixes some bug and change some word

--- PlannerApp/Views/Onboarding/OnboardingInfoViewController.swift
+++ PlannerApp/Views/Onboarding/OnboardingInfoViewController.swift
@@ -16,66 +16,83 @@ class OnboardingInfoViewController: ViewControllerProtocol ,UITextFieldDelegate{
     private static let descriptionFont = UIFont.ofSize(fontSize: 14, withType: .regular)
     
     fileprivate let items = [
-        OnboardingItemInfo(informationImage: UIImage(named:"landing3")!,
-                           title: "Agent",
-                           description: "We help Agents to organize their tasks by Category and Priority.",
-                           pageIcon: UIImage(named:"landing3")!,
-                           color: .white,
+        OnboardingItemInfo(informationImage: UIImage(named:"landing1")!,
+                           title: "_",
+                           description: "_",
+                           pageIcon: UIImage(named:"clear")!,
+                           color: .clear,
+                           titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont),
+        
+        OnboardingItemInfo(informationImage: UIImage(named:"landing2")!,
+                           title: "_",
+                           description: "_",
+                           pageIcon: UIImage(named:"clear")!,
+                           color: UIColor.clear,
                            titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont),
         
         OnboardingItemInfo(informationImage: UIImage(named:"landing3")!,
-                           title: "Business",
-                           description: "We have all features which Agents needed to fullfil and maximize his/her Business Management.",
-                           pageIcon: UIImage(named:"landing3")!,
-                           color: UIColor(red: 0.40, green: 0.69, blue: 0.71, alpha: 1.00),
-                           titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont),
-        
-        OnboardingItemInfo(informationImage: UIImage(named:"landing3")!,
-                           title: "Customer",
-                           description: "Make the Customers happy and satisfied.",
-                           pageIcon: UIImage(named:"landing3")!,
-                           color: UIColor(red: 0.61, green: 0.56, blue: 0.74, alpha: 1.00),
+                           title: "_",
+                           description: "_",
+                           pageIcon: UIImage(named:"clear")!,
+                           color: UIColor.clear,
                            titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont),
         
         ]
     
     let nameTextField: UITextField = UITextField()
     let onboarding = PaperOnboarding()
-    let imageLastImageView = UIImageView(image:UIImage(named: "man-woman-icon"))
-    let welcomeLabel = UILabel()
+    //let imageLastImageView = UIImageView(image:UIImage(named: "man-woman-icon"))
+    //let welcomeLabel = UILabel()
+    let pageControl = UIPageControl()
+    let welcomeImg = UIImageView(image: UIImage(named: "welcome"))
+    let bottomView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = UIColor(red: 0.33, green: 0.56, blue: 0.74, alpha: 1.00)
+        //view.backgroundColor = UIColor(red: 0.33, green: 0.56, blue: 0.74, alpha: 1.00)
+        view.backgroundColor = UIColor.white
+        welcomeImg.contentMode = .scaleAspectFit
+        bottomView.backgroundColor = .white
+        view.addSubview(welcomeImg)
+        view.addSubview(bottomView)
+        
+        pageControl.numberOfPages = 3
+        pageControl.backgroundColor = .white
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        pageControl.pageIndicatorTintColor = UIColor.yellow
+        bottomView.addSubview(pageControl)
         
         onboarding.delegate = self
         onboarding.dataSource = self
         onboarding.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(onboarding)
         
-        welcomeLabel.textAlignment = .center
-        welcomeLabel.text = "Welcome!"
-        welcomeLabel.font = OnboardingInfoViewController.titleFont
-        welcomeLabel.textColor = .white
-        welcomeLabel.isHidden = true
-        view.addSubview(welcomeLabel)
+//        welcomeLabel.textAlignment = .center
+//        welcomeLabel.text = "Welcome!"
+//        welcomeLabel.font = OnboardingInfoViewController.titleFont
+//        welcomeLabel.textColor = .white
+//        welcomeLabel.isHidden = true
+//        view.addSubview(welcomeLabel)
         
         nameTextField.delegate = self
         nameTextField.returnKeyType = .done
         nameTextField.keyboardType = .alphabet
+        nameTextField.backgroundColor = .white
+        nameTextField.backgroundColor?.withAlphaComponent(0.5)
         nameTextField.layer.borderColor = UIColor.white.cgColor
         nameTextField.layer.borderWidth = 1
         nameTextField.layer.cornerRadius = 10
         nameTextField.textAlignment = .center
         nameTextField.isHidden = true
         nameTextField.clipsToBounds = true
-        nameTextField.textColor = .white
-        nameTextField.placeholder = "What should I call you?"
+        nameTextField.textColor = CommonColor.darkGrayColor
+        nameTextField.placeholder = "How should i address you?"
+        //nameTextField.frame = CGRect(x: 70, y: 160, width: 195, height: <#T##Int#>)
         view.addSubview(nameTextField)
         
-        imageLastImageView.isHidden = true
-        view.addSubview(imageLastImageView)
+        //imageLastImageView.isHidden = true
+        //view.addSubview(imageLastImageView)
         
         view.updateConstraintsIfNeeded()
         view.setNeedsUpdateConstraints()
@@ -85,23 +102,39 @@ class OnboardingInfoViewController: ViewControllerProtocol ,UITextFieldDelegate{
         
         if !didSetupConstraints {
             
-            welcomeLabel.snp.makeConstraints{ make in
-                make.top.equalTo(view.safeArea.top).inset(30)
-                make.centerX.equalToSuperview()
+            welcomeImg.snp.makeConstraints { (make) in
+                make.top.bottom.left.right.equalTo(view).inset(0)
             }
+            
+//            welcomeLabel.snp.makeConstraints{ make in
+//                make.top.equalTo(view.safeArea.top).inset(30)
+//                make.centerX.equalToSuperview()
+//            }
             
             onboarding.snp.makeConstraints {make in
                 make.edges.equalToSuperview()
             }
-            imageLastImageView.snp.makeConstraints { make in
-                make.centerX.equalTo(nameTextField.snp.centerX)
-            }
+//            imageLastImageView.snp.makeConstraints { make in
+//                make.centerX.equalTo(nameTextField.snp.centerX)
+//            }
             
             nameTextField.snp.makeConstraints { make in
                 make.height.equalTo(40)
-                make.centerY.equalToSuperview()
-                make.left.right.equalTo(view).inset(20)
-                make.top.equalTo(imageLastImageView.snp.bottom).offset(20)
+                make.centerY.equalToSuperview().inset(-135)
+                //make.centerY.equalToSuperview().inset(-170)
+                make.left.right.equalTo(view).inset(75)
+                //make.top.equalTo(welcomeImg.snp.bottom).offset(20)
+            }
+            
+            bottomView.snp.makeConstraints { (make) in
+                make.left.right.top.bottom.equalTo(view).inset(0)
+            }
+            
+            pageControl.snp.makeConstraints { (make) in
+                make.left.right.equalTo(bottomView).inset(0)
+                make.height.equalTo(30)
+                make.centerX.equalTo(nameTextField.snp.centerX)
+                make.bottom.equalTo(bottomView).inset(10)
             }
             
             didSetupConstraints = true
@@ -115,12 +148,16 @@ class OnboardingInfoViewController: ViewControllerProtocol ,UITextFieldDelegate{
 extension OnboardingInfoViewController: PaperOnboardingDelegate {
     
     func onboardingDidTransitonToIndex(_ index: Int) {
+        
+        pageControl.currentPage = index
+        
         if index == self.items.count - 1{
             DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                self.onboarding.isHidden = true
+            self.onboarding.isHidden = true
                 self.nameTextField.isHidden = false
-                self.imageLastImageView.isHidden = false
-                self.welcomeLabel.isHidden = false
+                //self.imageLastImageView.isHidden = false
+                //self.welcomeLabel.isHidden = false
+                self.bottomView.isHidden = true
             }
         }
     }
@@ -129,11 +166,44 @@ extension OnboardingInfoViewController: PaperOnboardingDelegate {
         //item.titleLabel?.backgroundColor = .redColor()
         //item.descriptionLabel?.backgroundColor = .redColor()
         //item.imageView = ...
-        item.backgroundColor = .white
-        let screenSize: CGRect = UIScreen.main.bounds
+        item.backgroundColor = .clear
         
-        item.imageView?.frame = CGRect(x: 0, y: 185, width: screenSize.width, height: screenSize.height)
-        //item.imageView?.image = UIImage(named: "landing1")
+        let screenSize: CGRect = UIScreen.main.bounds
+        //item.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height - 40)
+        if screenSize.height <= 670
+        {
+            item.imageView?.frame = CGRect(x: 0, y: 160, width: screenSize.width, height: screenSize.height)
+//            if index == 0
+//            {
+//                item.imageView?.image = UIImage(named:"landing1")!
+//            }
+//            else if index == 1
+//            {
+//                item.imageView?.image = UIImage(named:"landing2")!
+//            }
+//            else
+//            {
+//                item.imageView?.image = UIImage(named:"landing3")!
+//            }
+        }
+        else
+        {
+            item.imageView?.frame = CGRect(x: 0, y: 100, width: screenSize.width, height: screenSize.height)
+//            if index == 0
+//            {
+//                item.imageView?.image = UIImage(named:"landingX1")!
+//            }
+//            else if index == 1
+//            {
+//                item.imageView?.image = UIImage(named:"landingX2")!
+//            }
+//            else
+//            {
+//                item.imageView?.image = UIImage(named:"landingX3")!
+//            }
+            
+        }
+        
         item.imageView?.contentMode = .scaleAspectFit
         item.imageView?.translatesAutoresizingMaskIntoConstraints = true
         
@@ -196,11 +266,11 @@ extension OnboardingInfoViewController: PaperOnboardingDataSource {
     //        return 2
     //    }
     //
-    //    func onboardingPageItemSelectedRadius() -> CGFloat {
-    //        return 10
-    //    }
+        func onboardingPageItemSelectedRadius() -> CGFloat {
+            return 10
+        }
         func onboardingPageItemColor(at index: Int) -> UIColor {
-            return [UIColor.darkGray, UIColor.darkGray, UIColor.darkGray][index]
+            return [UIColor.clear, UIColor.clear, UIColor.clear][index]
         }
     
     
